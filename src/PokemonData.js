37532b4e135d11d879b9626ab89bf1505f3ca362
Jack Goldstein <superuserperson4@gmail.com>
2017-05-30 21:04:57 -0400

    Add types to pokemon data

--- src/PokemonData.js
+++ src/PokemonData.js
@@ -6,6 +6,7 @@ class PokemonData extends Component {
         pokemon: {
             sprites: { front_default: ''},
             id: '',
+            types: {},
             name: '',
             height: '',
             weight: '',
@@ -22,12 +23,28 @@ class PokemonData extends Component {
         fetch(`http://pokeapi.co/api/v2/pokemon/${props.match.params.number}`)
             .then(response => response.json())
             .then(pokemon => this.setState({ pokemon }))
+            .catch(error => console.warn(error))
     }
 
     componentWillReceiveProps(nextProps) {
         const locationChanged = (nextProps.location !== this.props.location)
         if (locationChanged) {
             this.fetchUserData(nextProps)
+        }
+    }
+
+    pokemonTypes() {
+        let types = []
+        const { pokemon } = this.state
+
+        for (let i = 0; i < pokemon.types.length; i++){
+            types.push(pokemon.types[i].type.name)
+        }
+
+        if (types.length === 1) {
+            return <h3>Types: {types[0]}</h3>
+        } else {
+            return <h3>Types: {types[0]} & {types[1]}</h3>
         }
     }
 
@@ -38,6 +55,7 @@ class PokemonData extends Component {
                 <img src={pokemon.sprites.front_default} alt="pokemon"/>
                 <h2>Pokemon #{pokemon.id}: </h2>
                 <h1>{pokemon.name}</h1>
+                <h3>{this.pokemonTypes()}</h3>
                 <h3>Height: {pokemon.height}</h3>
                 <h3>Weight: {pokemon.weight}</h3>
                 <h3>Base Experience: {pokemon.base_experience}</h3>
