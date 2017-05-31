bb7a763a8a7ea06b63f52f2fe532b13be069246a
Jack Goldstein <superuserperson4@gmail.com>
2017-05-31 09:41:39 -0400

    Make a few style and syntax changes

--- src/PokemonData.js
+++ src/PokemonData.js
@@ -42,7 +42,7 @@ class PokemonData extends Component {
         }
 
         if (types.length === 1) {
-            return <h3>Types: {types[0]}</h3>
+            return <h3>Type: {types[0]}</h3>
         } else {
             return <h3>Types: {types[1]} & {types[0]}</h3>
         }
